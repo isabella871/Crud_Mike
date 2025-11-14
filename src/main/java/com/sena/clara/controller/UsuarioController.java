37f66eebaecce4e88632511995be3672126561f5
Isabella Bernal <bellabernalcarvajal871@gmail.com>
2025-11-13 23:09:12 -0500

    crud en springBoot

--- src/main/java/com/sena/clara/controller/UsuarioController.java
+++ src/main/java/com/sena/clara/controller/UsuarioController.java
@@ -4,8 +4,14 @@ import com.sena.clara.dto.UsuarioRequestDTO;
 import com.sena.clara.dto.UsuarioResponseDTO;
 import com.sena.clara.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +23,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
-    @PostMapping("crear")
+    @PostMapping("crear") // endpoints => postman
     public ResponseEntity<UsuarioResponseDTO> crearUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO){
         UsuarioResponseDTO response = usuarioService.crearUsuario(usuarioRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("listar")
+    public ResponseEntity<List<UsuarioResponseDTO>> listarUsuario(){
+        List<UsuarioResponseDTO> response = usuarioService.ListarUsuarios();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarUserID(@PathVariable Long id){
+        UsuarioResponseDTO usuario = usuarioService.buscarUserID(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> eliminarUsuario(@PathVariable Long id){
+        usuarioService.eliminarUsuario(id);
+        return ResponseEntity.ok("Usuario eliminado correctamente");
     }
 }
--- src/main/java/com/sena/clara/service/UsuarioService.java
+++ src/main/java/com/sena/clara/service/UsuarioService.java
@@ -4,7 +4,13 @@ import com.sena.clara.dto.UsuarioRequestDTO;
 import com.sena.clara.dto.UsuarioResponseDTO;
 import com.sena.clara.entity.Usuario;
 import com.sena.clara.repository.UsuarioRepository;
+
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,4 +35,40 @@ public class UsuarioService {
         return response;
     }
 
+    @Transactional
+    public List<UsuarioResponseDTO> ListarUsuarios(){
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        List<UsuarioResponseDTO> response = new ArrayList<>();
+
+        for (Usuario usuario: usuarios){
+            UsuarioResponseDTO responseDTO = new UsuarioResponseDTO();
+            responseDTO.setId(usuario.getId());
+            responseDTO.setNombre(usuario.getNombre());
+            responseDTO.setEdad(usuario.getEdad());
+            responseDTO.setCorreo(usuario.getCorreo());
+
+            response.add(responseDTO);
+        }
+        return response;
+    }
+
+    public UsuarioResponseDTO buscarUserID(Long id){
+        Usuario usuario = usuarioRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Usuario no encontado"));
+        
+        UsuarioResponseDTO responseDTO = new UsuarioResponseDTO();
+        responseDTO.setId(usuario.getId());
+        responseDTO.setNombre(usuario.getNombre());
+        responseDTO.setEdad(usuario.getEdad());
+        responseDTO.setCorreo(usuario.getCorreo());
+
+        return responseDTO;
+    }
+
+    public void eliminarUsuario(Long id){
+        Usuario usuario = usuarioRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("El usuario no ha sido encontrado"));
+
+        usuarioRepository.delete(usuario);
+    }
 }
